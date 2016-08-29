4fa597c20099aaa552f3288a2e3d299bf280a3d8
jeroendelcour <jeroendelcour@gmail.com>
2016-08-29 17:05:59 -0400

    tweaked original 2016 Olympics plots, added historical interactive plot with data since 1896

---
+++ public/d3jsgraphs/blog/olympics/flags/flag_packs.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Flag Packs - Free Country Flags, All the flags in the world</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Language" content="en-US" />
+<meta name="description" content="Free country world flags. Download packs of country flags in different styles and sizes for free. Search for specific country flags. Download snippets for country dropdown lists. All the countries of the world." />
+<meta name="keywords" content="free flags, country flags, flags, flags download, countries, world flags, download flags, flags icons, free downloads, national flag, nation, flag packs, all countries, all country flags, free country flags" />
+<link rel="stylesheet" type="text/css" href="stylesheet.css?date=201007171635" />
+</head>
+<body>
+	<div id="header">
+        <h1><span>Free Country Flags</span></h1>
+    </div>
+	<div id="navigation">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="flag_packs.php">Flag Packs</a></li>
+			<li><a href="countries.php">Flags by Country</a></li>
+			<li><a href="code_snippets.php">Utilities</a></li>
+			<li><a href="contact.php">Contact</a></li>
+		</ul>
+		<div class="footerclear"></div>
+	</div>
+	<div id="content">
+					<script type="text/javascript"><!--
+google_ad_client = "pub-8275061180575141";
+//Flags Top Banner
+google_ad_slot = "3297460572";
+google_ad_width = 728;
+google_ad_height = 90;
+//--></script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+			<div id="packs">
+				<h1>Download country flags</h1>
+				<h2>All packs available in different sizes.<br />
+				More than 220 world nations included.</h2>
+				<div class="pack">
+					<h1><a href="flag_pack.php?id=1">Pack 1</a></h1>
+					<p>Example:</p>
+					<a href="flag_pack.php?id=1"><img src="countries/Portugal/1/small/Portugal.png" alt="Portugal" class="small" /></a><br />
+					<a href="flag_pack.php?id=1"><img src="countries/France/1/small/France.png" alt="France" class="small" /></a><br />
+					<a href="flag_pack.php?id=1"><img src="countries/United_States_of_America/1/small/United_States_of_America.png" alt="United States of America" class="small last" /></a><br />
+				</div>
+				<div class="pack">
+					<h1><a href="flag_pack.php?id=2">Pack 2</a></h1>
+					<p>Example:</p>
+					<a href="flag_pack.php?id=2"><img src="countries/Portugal/2/small/Portugal.png" alt="Portugal" class="small" /></a><br />
+					<a href="flag_pack.php?id=2"><img src="countries/France/2/small/France.png" alt="France" class="small" /></a><br />
+					<a href="flag_pack.php?id=2"><img src="countries/United_States_of_America/2/small/United_States_of_America.png" alt="United States of America" class="small last" /></a><br />
+				</div>
+				<div class="pack">
+					<h1><a href="flag_pack.php?id=3">Pack 3</a></h1>
+					<p>Example:</p>
+					<a href="flag_pack.php?id=3"><img src="countries/Portugal/3/small/Portugal.png" alt="Portugal" class="small" /></a><br />
+					<a href="flag_pack.php?id=3"><img src="countries/France/3/small/France.png" alt="France" class="small" /></a><br />
+					<a href="flag_pack.php?id=3"><img src="countries/United_States_of_America/3/small/United_States_of_America.png" alt="United States of America" class="small last" /></a><br />
+				</div>
+				<div class="pack endpack">
+					<h1><a href="flag_pack.php?id=4">Pack 4</a></h1>
+					<p>Example:</p>
+					<a href="flag_pack.php?id=4"><img src="countries/Portugal/4/small/Portugal.png" alt="Portugal" class="small" /></a><br />
+					<a href="flag_pack.php?id=4"><img src="countries/France/4/small/France.png" alt="France" class="small" /></a><br />
+					<a href="flag_pack.php?id=4"><img src="countries/United_States_of_America/4/small/United_States_of_America.png" alt="United States of America" class="small last" /></a><br />
+				</div>
+				<div class="footerclear"></div>
+			</div>
+			<script type="text/javascript"><!--
+google_ad_client = "pub-8275061180575141";
+//Flags Bottom Banner
+google_ad_slot = "2000543999";
+google_ad_width = 728;
+google_ad_height = 90;
+//--></script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+		<br />
+		<div id="license">
+			<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/StillImage" property="dct:title" rel="dct:type">Free Country Flags</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.free-country-flags.com/" property="cc:attributionName" rel="cc:attributionURL">Gang of the Coconuts</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.
+		</div>
+	</div>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-3379164-1']);
+	  _gaq.push(['_setSiteSpeedSampleRate', 100]);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+</body>
+</html>
